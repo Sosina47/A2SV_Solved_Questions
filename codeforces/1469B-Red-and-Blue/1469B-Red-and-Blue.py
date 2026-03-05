087e681637b2dc1c6be1be2c29da4c2754c0e1f1
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-03-05 12:26:34 +0300

    Update 1469B-Red-and-Blue.py

--- codeforces/1469B-Red-and-Blue/1469B-Red-and-Blue.py
+++ codeforces/1469B-Red-and-Blue/1469B-Red-and-Blue.py
@@ -6,50 +6,8 @@ for _ in range(int(input())):
 
     len_b = int(input())
     blue = list(accumulate(list(map(int, input().split()))))
-    output = [] 
-    r = b = 0
-    max_ = 0
 
-    for r in range(len_r):
-        max_ = max(max_, red[r])
-        for b in range(len_b):
-            max_ = max(max_, blue[b])
-            max_ = max(max_, red[r] + blue[b])
+    max_blue = max(blue)
+    max_red = max(red)
     
-    print(max_)
-    
-    
-    
-    
-    
-    
-
-    # while r < len_r and b < len_b:
-    #     if red[r] >= 0 and red[r] > blue[b]:
-    #         output.append(red[r])
-    #         r += 1
-
-    #     elif blue[b] >= 0 and blue[b] > red[r]:
-    #         output.append(blue[b])
-    #         b += 1
-
-    #     elif suffix_blue[b] > suffix_red[r]:
-    #         output.append(blue[b])
-    #         b += 1
-    #     elif suffix_red[r] > suffix_blue[b]:
-    #         output.append(red[r])
-    #         r += 1
-
-    #     else:
-    #         if red[r] > blue[b]:
-    #             output.append(red[r])
-    #             r += 1
-    #         else:
-    #             output.append(blue[b])
-    #             b += 1
-
-    # output.extend(red[r:])
-    # output.extend(blue[b:])
-    
-    # output = list(accumulate(output))
-    # print(max(0, max(output)))
+    print(max([0, max_blue, max_red, max_blue + max_red]))
